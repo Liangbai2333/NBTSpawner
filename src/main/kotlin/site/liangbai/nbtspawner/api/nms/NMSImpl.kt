59ae90d0ab1882dca44394bcacf7e724d67b8a1c
Liangbai2333 <admin@liangbai.site>
2021-08-25 15:11:18 +0800

    Support more version

--- src/main/kotlin/site/liangbai/nbtspawner/api/nms/NMSImpl.kt
+++ src/main/kotlin/site/liangbai/nbtspawner/api/nms/NMSImpl.kt
@@ -27,35 +27,70 @@ import site.liangbai.nbtspawner.api.nms.factory.AbstractNBTFactory
 import site.liangbai.nbtspawner.api.nms.factory.NBTTagFactory
 import site.liangbai.nbtspawner.api.nms.factory.NBTTagListFactory
 import taboolib.common.reflect.Reflex.Companion.getProperty
+import taboolib.common.reflect.Reflex.Companion.invokeConstructor
+import taboolib.common.reflect.Reflex.Companion.invokeMethod
 import taboolib.module.nms.MinecraftVersion
 
 class NMSImpl : NMS() {
     private val isUniversal = MinecraftVersion.isUniversal
 
+    private val majorLegacy = MinecraftVersion.majorLegacy
+
+    @Throws(IllegalStateException::class)
     override fun readEntity(entity: Entity): AbstractNBTFactory<String> {
         val craftEntity = entity as CraftEntity
         val nbt  = NBTTagCompound().also {
-            craftEntity.handle.save(it)
+            if (majorLegacy >= 11200) {
+                craftEntity.handle.save(it)
+            } else if (majorLegacy in 10800..11200) {
+                craftEntity.handle.invokeMethod<Any>("e", it)
+            } else {
+                throw IllegalStateException("unsupported minecraft version $majorLegacy")
+            }
         }
         return findFactory(nbt)
     }
 
+    @Throws(IllegalArgumentException::class)
     override fun readItemStack(itemStack: ItemStack): AbstractNBTFactory<String> {
-        val nmsItemStack = CraftItemStack.asNMSCopy(itemStack)
-        val nbt = nmsItemStack.tag ?: NBTTagCompound()
-        return findFactory(nbt)
+        if (itemStack is CraftItemStack) {
+            val nmsItemStack = itemStack.handle ?: throw IllegalArgumentException("could load NBT from empty item stack")
+
+            return findFactory(nmsItemStack.tag ?: NBTTagCompound())
+        } else {
+            val nmsItemStack = CraftItemStack.asNMSCopy(itemStack)
+            return findFactory(nmsItemStack.tag ?: NBTTagCompound())
+        }
     }
 
+    @Throws(IllegalStateException::class)
     override fun writeEntity(entity: Entity, nbtFactory: AbstractNBTFactory<String>) {
         val craftEntity = entity as CraftEntity
 
         craftEntity.handle.also {
-            it.load(nbtFactory.handle as NBTTagCompound)
+            if (majorLegacy >= 11600) {
+                it.load(nbtFactory.handle as NBTTagCompound)
+            } else if (majorLegacy in 10800..11600) {
+                it.invokeMethod<Any>("f", nbtFactory.handle)
+            } else {
+                throw IllegalStateException("unsupported minecraft version $majorLegacy")
+            }
         }
     }
 
     override fun writeItemStack(itemStack: ItemStack, nbtFactory: AbstractNBTFactory<String>) {
-        TODO("Not yet implemented")
+        if (itemStack is CraftItemStack) {
+            val nmsItemStack = itemStack.handle
+
+            if (nmsItemStack != null) {
+                nmsItemStack.tag = nbtFactory.handle as NBTTagCompound
+            }
+        } else {
+            val nmsItemStack = CraftItemStack.asNMSCopy(itemStack)
+            nmsItemStack.tag = nbtFactory.handle as NBTTagCompound
+            val copiedItemStack = CraftItemStack.asBukkitCopy(nmsItemStack)
+            itemStack.setItemMeta(copiedItemStack.itemMeta)
+        }
     }
 
     @Throws(IllegalArgumentException::class)
@@ -85,23 +120,46 @@ class NMSImpl : NMS() {
         }
     }
 
-    @Throws(IllegalArgumentException::class)
+    @Throws(IllegalArgumentException::class, IllegalStateException::class)
     override fun wrappedAsNBTBase(value: Any): Any {
-        return value.run { when (this) {
-            is NBTBase -> return this
-            is AbstractNBTFactory<*> -> this.handle
-            is String -> NBTTagString.a(this)
-            is Boolean -> NBTTagByte.a(this)
-            is Byte -> NBTTagByte.a(this)
-            is Int -> NBTTagInt.a(this)
-            is Long -> NBTTagLong.a(this)
-            is Short -> NBTTagShort.a(this)
-            is Double -> NBTTagDouble.a(this)
-            is Float -> NBTTagFloat.a(this)
-            is ByteArray -> NBTTagByteArray(this)
-            is IntArray -> NBTTagIntArray(this)
-            is LongArray -> NBTTagLongArray(this)
-            else -> throw IllegalArgumentException("could not wrapped the type: ${this::class.java.simpleName} as any nbt type.")
+        return value.run { when (majorLegacy) {
+            in 11500..99999 -> when (this) {
+                is NBTBase -> return this
+                is AbstractNBTFactory<*> -> this.handle
+                is String -> NBTTagString.a(this)
+                is Boolean -> NBTTagByte.a(this)
+                is Byte -> NBTTagByte.a(this)
+                is Int -> NBTTagInt.a(this)
+                is Long -> NBTTagLong.a(this)
+                is Short -> NBTTagShort.a(this)
+                is Double -> NBTTagDouble.a(this)
+                is Float -> NBTTagFloat.a(this)
+                is ByteArray -> NBTTagByteArray(this)
+                is IntArray -> NBTTagIntArray(this)
+                is LongArray -> NBTTagLongArray(this)
+                else -> throw IllegalArgumentException("could not wrapped the type: ${this::class.java.simpleName} as any nbt type.")
+            }
+            in 10800..11500 -> when (this) {
+                is NBTBase -> return this
+                is AbstractNBTFactory<*> -> this.handle
+                is String -> NBTTagString::class.java.invokeConstructor(this)
+                is Boolean -> NBTTagByte::class.java.invokeConstructor(this)
+                is Byte -> NBTTagByte::class.java.invokeConstructor(this)
+                is Int -> NBTTagInt::class.java.invokeConstructor(this)
+                is Long -> NBTTagLong::class.java.invokeConstructor(this)
+                is Short -> NBTTagShort::class.java.invokeConstructor(this)
+                is Double -> NBTTagDouble::class.java.invokeConstructor(this)
+                is Float -> NBTTagFloat::class.java.invokeConstructor(this)
+                is ByteArray -> NBTTagByteArray::class.java.invokeConstructor(this)
+                is IntArray -> NBTTagIntArray::class.java.invokeConstructor(this)
+                is LongArray -> {
+                    if (majorLegacy >= 11200) {
+                        NBTTagLongArray::class.java.invokeConstructor(this)
+                    } else throw IllegalStateException("unsupported minecraft version $majorLegacy for Long Array type.")
+                }
+                else -> throw IllegalArgumentException("could not wrapped the type: ${this::class.java.simpleName} as any nbt type.")
+            }
+            else -> throw IllegalStateException("unsupported minecraft version $majorLegacy")
         } }
     }
 }
